a709f6408336c9007f25a2cd673f8f90ae95c332
Sebastian Grigor <rezervat24@yahoo.com>
2019-04-08 00:37:54 +0300

    Add Travis CI

--- SmartphoneCompanionWeb/src/environments/environment.prod.ts
+++ SmartphoneCompanionWeb/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDepx6Ghyx-H33gd8jQKEuXo8ZLwsijQQQ',
+    authDomain: 'smartphone-companion.firebaseapp.com',
+    databaseURL: 'https://smartphone-companion.firebaseio.com',
+    projectId: 'smartphone-companion',
+    storageBucket: 'smartphone-companion.appspot.com',
+    messagingSenderId: '329891444276'
+  }
 };
