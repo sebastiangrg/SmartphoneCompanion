e2890c50896e6eeb470dd3f2a7f0100bc52a3002
Sebastian Grigor <rezervat24@yahoo.com>
2019-04-12 22:27:26 +0300

    Add country code selection

--- SmartphoneCompanionAndroid/app/src/main/java/com/project/smartphonecompanionandroid/ui/fragments/PhoneNumberFragment.kt
+++ SmartphoneCompanionAndroid/app/src/main/java/com/project/smartphonecompanionandroid/ui/fragments/PhoneNumberFragment.kt
@@ -5,11 +5,13 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
+import com.hbb20.CountryCodePicker
 import com.project.smartphonecompanionandroid.R
 import com.project.smartphonecompanionandroid.utils.replaceWith
 
 import kotlinx.android.synthetic.main.fragment_phone_number.*
+import kotlinx.android.synthetic.main.fragment_verification_code.*
+
 
 class PhoneNumberFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -21,24 +23,20 @@ class PhoneNumberFragment : Fragment() {
 
         nextButton.setOnClickListener { submitPhoneNumber() }
 
-        phoneNumberEditText.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_NEXT) {
-                submitPhoneNumber()
-                true
-            } else {
-                false
-            }
-        }
+        countryCodePicker.registerCarrierNumberEditText(carrierNumberEditText)
+        countryCodePicker.setDefaultCountryUsingNameCode("RO")
+
+        countryCodePicker.setPhoneNumberValidityChangeListener(CountryCodePicker.PhoneNumberValidityChangeListener {
+            nextButton.isEnabled = it
+        })
     }
 
     private fun submitPhoneNumber() {
-        val phoneNumber = phoneNumberEditText.text.toString()
-        if (phoneNumber.isEmpty() || phoneNumber.length < 10) {
-            phoneNumberEditText.error = "Enter a valid phone number"
-            phoneNumberEditText.requestFocus()
-            return
+        val phoneNumber = countryCodePicker.fullNumberWithPlus
+
+        if (countryCodePicker.isValidFullNumber) {
+            goToVerification(phoneNumber)
         }
-        goToVerification(phoneNumber)
     }
 
     private fun goToVerification(phoneNumber: String) {
