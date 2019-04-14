e177df8efb9bef1abfaf18715645646ad5aa0cdc
Sebastian Grigor <rezervat24@yahoo.com>
2019-04-14 21:51:38 +0300

    Add phone number confirmation dialog

--- SmartphoneCompanionAndroid/app/src/main/java/com/project/smartphonecompanionandroid/ui/fragments/PhoneNumberFragment.kt
+++ SmartphoneCompanionAndroid/app/src/main/java/com/project/smartphonecompanionandroid/ui/fragments/PhoneNumberFragment.kt
@@ -1,17 +1,18 @@
 package com.project.smartphonecompanionandroid.ui.fragments
 
-import android.opengl.Visibility
 import android.os.Bundle
-import com.google.android.material.button.MaterialButton
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.hbb20.CountryCodePicker
-import com.project.smartphonecompanionandroid.R
 import com.project.smartphonecompanionandroid.utils.replaceWith
 
 import kotlinx.android.synthetic.main.fragment_phone_number.*
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.core.text.HtmlCompat
+import com.project.smartphonecompanionandroid.R
 
 
 class PhoneNumberFragment : Fragment() {
@@ -22,7 +23,7 @@ class PhoneNumberFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        nextButton.setOnClickListener { submitPhoneNumber() }
+        nextButton.setOnClickListener { showConfirmationDialog() }
 
         countryCodePicker.registerCarrierNumberEditText(phoneNumberEditText)
         countryCodePicker.setDefaultCountryUsingNameCode("RO")
@@ -33,21 +34,37 @@ class PhoneNumberFragment : Fragment() {
         })
     }
 
+    private fun showConfirmationDialog() {
+        val builder = AlertDialog.Builder(ContextThemeWrapper(requireContext(), R.style.Dialog))
+
+        with(builder)
+        {
+            setTitle(getText(R.string.number_confirmation_title))
+            setMessage(
+                HtmlCompat.fromHtml(
+                    getString(
+                        R.string.number_confirmation_message,
+                        countryCodePicker.fullNumberWithPlus
+                    ),
+                    HtmlCompat.FROM_HTML_MODE_LEGACY
+                )
+            )
+            setPositiveButton(android.R.string.ok) { _, _ -> run { submitPhoneNumber() } }
+            setNegativeButton(android.R.string.cancel) { _, _ -> run {} }
+        }.show()
+    }
+
     private fun submitPhoneNumber() {
         val phoneNumber = countryCodePicker.fullNumberWithPlus
 
         if (countryCodePicker.isValidFullNumber) {
-            goToVerification(phoneNumber)
-        }
-    }
+            val fragment = VerificationCodeFragment().apply {
+                val bundle = Bundle()
+                bundle.putString("phoneNumber", phoneNumber)
+                arguments = bundle
+            }
 
-    private fun goToVerification(phoneNumber: String) {
-        val fragment = VerificationCodeFragment().apply {
-            val bundle = Bundle()
-            bundle.putString("phoneNumber", phoneNumber)
-            arguments = bundle
+            requireActivity().replaceWith(fragment)
         }
-
-        requireActivity().replaceWith(fragment)
     }
 }
